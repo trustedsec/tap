bf3c98be801f4e1db9eed11e8d0b8059758884a6
TrustedSec <info@trustedsec.com>
2015-11-18 15:18:55 +0000

    add full tap path

--- scripts/manual_update.py
+++ scripts/manual_update.py
@@ -3,5 +3,5 @@ import subprocess
 import os
 # only run this when you are in the scripts directory 
 if os.path.isfile("tap/"): subprocess.Popen("rm -rf tap/", shell=True).wait()
-subprocess.Popen("git clone https://github.com/trustedsec/tap;cp -rf tap/* ../;rm -rf tap/", shell=True).wait()
+subprocess.Popen("git clone https://github.com/trustedsec/tap;cp -rf tap/* /usr/share/tap/;rm -rf tap/", shell=True).wait()
 
