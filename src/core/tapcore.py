f4ad39d04bc3db43ab5234ea6061bcbb8521245e
Carlos Perez <carlos_perez@darkoperator.com>
2020-03-04 15:50:02 -0400

    Indentation and Py3 fixes tapcore.py
    Indentation and Py3 fixes tapcore.py

--- src/core/tapcore.py
+++ src/core/tapcore.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 ###############################
 #
 # main functions for TAP
@@ -41,7 +42,7 @@ def encryptAES(data):
     cipher = AES.new(secret)
 
     aes = EncodeAES(cipher, data)
-    fileopen = file("/usr/share/tap/config", "r")
+    fileopen = open("/usr/share/tap/config", "r")
     config = ""
     for line in fileopen:
         line = line.rstrip()
@@ -50,10 +51,10 @@ def encryptAES(data):
 
         config = config + line + "\n"
     secret = base64.b64encode(secret)
-    filewrite = file("/root/.tap/store", "w")
+    filewrite = open("/root/.tap/store", "w")
     filewrite.write(secret)
     filewrite.close()
-    filewrite = file("/usr/share/tap/config", "w")
+    filewrite = open("/usr/share/tap/config", "w")
     filewrite.write(config)
     filewrite.close()    
     subprocess.Popen("/etc/init.d/ssh restart", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
@@ -63,35 +64,35 @@ def decryptAES(data):
     
     if os.path.isfile("/root/.tap/store"):
 
-    	    # the character used for padding--with a block cipher such as AES, the value
-    	    # you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
-    	    # used to ensure that your value is always a multiple of BLOCK_SIZE
-            PADDING = '{'
-	
-	    BLOCK_SIZE = 32
-	
-	    # one-liner to sufficiently pad the text to be encrypted
-	    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-	
-	    # random value here to randomize builds
-	    a = 50 * 5
-	
-	    # one-liners to encrypt/encode and decrypt/decode a string
-	    # encrypt with AES, encode with base64
-	    DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-	    fileopen = file("/root/.tap/store", "r")
-	    key = fileopen.read()
-	    secret = base64.b64decode(key)
-	    cipher = AES.new(secret)
-	    aes = DecodeAES(cipher, data)
-	    return str(aes)
+        # the character used for padding--with a block cipher such as AES, the value
+        # you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
+        # used to ensure that your value is always a multiple of BLOCK_SIZE
+        PADDING = '{'
+    
+        BLOCK_SIZE = 32
+    
+        # one-liner to sufficiently pad the text to be encrypted
+        pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+    
+        # random value here to randomize builds
+        a = 50 * 5
+    
+        # one-liners to encrypt/encode and decrypt/decode a string
+        # encrypt with AES, encode with base64
+        DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+        fileopen = open("/root/.tap/store", "r")
+        key = fileopen.read()
+        secret = base64.b64decode(key)
+        cipher = AES.new(secret)
+        aes = DecodeAES(cipher, data)
+        return str(aes)
 
     else: return ""
 
 # quick check to see if we are running ubuntu-linux
 def check_debian():
     if os.path.isfile("/etc/apt/sources.list"):
-	return "Debian"
+        return "Debian"
     else:
         print("[!] Not running a Debian variant..")
         return "Non-Debian"
@@ -99,12 +100,12 @@ def check_debian():
 # check keepalive
 def check_keepalive():
     if os.path.isfile("/etc/ssh/ssh_config"):
-        fileopen = file("/etc/ssh/ssh_config", "r")
+        fileopen = open("/etc/ssh/ssh_config", "r")
         data = fileopen.read()
         match = re.search("ServerAliveInterval", data)
         if not match:
             print("[*] Adding Keepalive info to /etc/ssh/ssh_config...")
-            filewrite = file("/etc/ssh/ssh_config", "a")
+            filewrite = open("/etc/ssh/ssh_config", "a")
             filewrite.write("ServerAliveInterval 15\n")
             filewrite.write("ServerAliveCountMax 4\n")
             filewrite.close()
@@ -147,7 +148,7 @@ def proxychain():
     if socks != "":
             if os.path.isfile("/etc/proxychains.conf"):
                 os.remove("/etc/proxychains.conf")
-            filewrite = file("/etc/proxychains.conf", "w")
+            filewrite = open("/etc/proxychains.conf", "w")
             filewrite.write("strict_chain\nproxy_dns\ntcp_read_time_out 15000\ntcp_connect_time_out 8000\n[ProxyList]\n\nsocks5 127.0.0.1 %s" % (socks))
             filewrite.close()
 
@@ -158,8 +159,8 @@ def tap_update():
         print("[*] Updating TAP now with the latest TAP codebase")
         updates = check_config("UPDATE_SERVER=")
         if not os.path.isdir("/usr/share/tap"):
-	    subprocess.Popen("git clone https://github.com/trustedsec/tap", shell=True).wait()
-	os.chdir("/usr/share/tap")
+            subprocess.Popen("git clone https://github.com/trustedsec/tap", shell=True).wait()
+        os.chdir("/usr/share/tap")
         subprocess.Popen(updates, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 
     else:
@@ -179,7 +180,7 @@ def check_config_path():
 def check_config(param):
         # grab the default path
         path = check_config_path()
-        fileopen = file(path, "r")
+        fileopen = open(path, "r")
         # iterate through lines in file
         counter = 0
         for line in fileopen:
@@ -222,13 +223,13 @@ def ssh_run():
     if ssh_gen.lower() == "on":
         ssh_commands = "-i /root/.ssh/id_rsa"
     try:
-	    child = pexpect.spawn("ssh-add")
-	    i = child.expect(['pass'])
+        child = pexpect.spawn("ssh-add")
+        i = child.expect(['pass'])
 
-	    # if prompting for password
-	    if i == 0:
-	        child.sendline(password)
-   	        child.close()
+        # if prompting for password
+        if i == 0:
+            child.sendline(password)
+            child.close()
 
     except: pass
 
@@ -259,7 +260,7 @@ def ssh_run():
     if i == 1:
         child.sendline("yes")
         #if ssh_gen.lower() == "off":
-	if password != "":
+    if password != "":
             # added an except here to wait for it so the password doesn't trigger prompting invalid password
             child.expect(['pass'])
             # send the password
@@ -275,57 +276,57 @@ def ssh_run():
        # this is for SSH only
         print("[*] Fail-safe SSH is active.. Monitoring SSH connections. - All is well.")
         time.sleep(1)
-	try:
-	        portcheck = pexpect.spawn('ssh -p %s %s %s@%s netstat -an | egrep "tcp.*:%s.*LISTEN"' % (port, ssh_commands, username, host, localport))
-	        i = portcheck.expect(['pass', 'want to continue connecting', localport])
-	        # if prompting for password
-	        if i == 0:
-	            portcheck.sendline(password)
-	
-	        # if wanting to accept certificate for new ssh
-	        if i == 1:
-	            portcheck.sendline("yes")
-	            #if ssh_gen.lower() == "off":
-	
-	        #if ssh_gen.lower() == "off":
-		    if password != "":
-	                portcheck.expect("password")
-	                portcheck.sendline(password)
-	
-	        # if we logged in already for some reason - shouldnt hit this
-	        if i == 2:
-	            # need to re-intiate to pass through   
-	            portcheck.sendline("echo alive")
-	
-	        i = portcheck.expect([localport, "alive"])
-	
-		if i == 0:
-       	        	# keep alive
-       	        	portcheck.sendline("echo alive")	
+    try:
+        portcheck = pexpect.spawn('ssh -p %s %s %s@%s netstat -an | egrep "tcp.*:%s.*LISTEN"' % (port, ssh_commands, username, host, localport))
+        i = portcheck.expect(['pass', 'want to continue connecting', localport])
+        # if prompting for password
+        if i == 0:
+            portcheck.sendline(password)
 
-            	# if we already hit here
-            	if i == 1:
-			pass
+        # if wanting to accept certificate for new ssh
+        if i == 1:
+            portcheck.sendline("yes")
+            #if ssh_gen.lower() == "off":
 
-        except:
-	
-            print("\n[*] Reinitializing SSH tunnel - it went down apparently\n")
-            subprocess.Popen("rm /root/.ssh/known_hosts", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            child = pexpect.spawn("ssh -R %s:127.0.0.1:22 %s@%s -p %s %s" % (localport,username,host,port, ssh_commands))
-            i = child.expect(['pass', 'want to continue connecting', localport])
-            if i == 0:
+        #if ssh_gen.lower() == "off":
+        if password != "":
+            portcheck.expect("password")
+            portcheck.sendline(password)
+
+        # if we logged in already for some reason - shouldnt hit this
+        if i == 2:
+            # need to re-intiate to pass through   
+            portcheck.sendline("echo alive")
+
+        i = portcheck.expect([localport, "alive"])
+    
+        if i == 0:
+            # keep alive
+            portcheck.sendline("echo alive")	
+
+        # if we already hit here
+        if i == 1:
+            pass
+
+    except:
+    
+        print("\n[*] Reinitializing SSH tunnel - it went down apparently\n")
+        subprocess.Popen("rm /root/.ssh/known_hosts", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        child = pexpect.spawn("ssh -R %s:127.0.0.1:22 %s@%s -p %s %s" % (localport,username,host,port, ssh_commands))
+        i = child.expect(['pass', 'want to continue connecting', localport])
+        if i == 0:
+            child.sendline(password)
+
+        if i == 1:
+            child.sendline("yes")
+            if password != "":
+                child.expect("pass")
                 child.sendline(password)
 
-            if i == 1:
-                child.sendline("yes")
-                if password != "":
-                    child.expect("pass")
-                    child.sendline(password)
+        if i == 2:
+            child.sendline("echo alive")
 
-	    if i == 2:
-		child.sendline("echo alive")
-
-            print("[*] Back up and running. Waiting and checking.....")
+        print("[*] Back up and running. Waiting and checking.....")
 
         # initiate socks proxy
         socks = check_config("SOCKS_PROXY_PORT=").rstrip()
@@ -334,23 +335,23 @@ def ssh_run():
             stdout_value = proc.stdout.read()
             if not "127.0.0.1:" in stdout_value:
                 print("[*] Establishing socks proxy and tunneling 80/443 traffic")
-		try:
-                	child1 = pexpect.spawn("ssh -D %s %s@%s -p %s %s" % (socks,username,host,port, ssh_commands))
-                	i = child1.expect(['pass', 'want to continue connecting', 'Last login:'])
-                	if i == 0:
-                    		child1.sendline(password)
-                	if i == 1:
-                    		child1.sendline("yes")
-		    	if password != "":
-	                        child1.expect("pass")
-	                        child1.sendline(password)
+        try:
+            child1 = pexpect.spawn("ssh -D %s %s@%s -p %s %s" % (socks,username,host,port, ssh_commands))
+            i = child1.expect(['pass', 'want to continue connecting', 'Last login:'])
+            if i == 0:
+                    child1.sendline(password)
+            if i == 1:
+                child1.sendline("yes")
+            if password != "":
+                child1.expect("pass")
+                child1.sendline(password)
 
-			if i == 2: pass
+            if i == 2: pass
 
-		except Exception as e:
-			print(e)
-			print ("[!] Unable to establish a socks proxy - moving on.")
-			pass
+        except Exception as e:
+            print(e)
+            print ("[!] Unable to establish a socks proxy - moving on.")
+            pass
 
         # wait and sleep
         time.sleep(interval)
@@ -373,13 +374,13 @@ def execute_command():
                         # here we check first to see if we need to execute or we have already
                         commands = 0
                         if os.path.isfile("/tmp/tap.txt"):
-                            filewrite = file("/tmp/tap_comp.txt", "w")
+                            filewrite = open("/tmp/tap_comp.txt", "w")
                             filewrite.write(html)
                             filewrite.close()
                             # here we do hash comparisons
-                            fileopen1 = file("/tmp/tap.txt", "r")
+                            fileopen1 = open ("/tmp/tap.txt", "r")
                             # our compare file
-                            fileopen2 = file("/tmp/tap_comp.txt", "r")
+                            fileopen2 = open("/tmp/tap_comp.txt", "r")
                             data1 = fileopen1.read()
                             data2 = fileopen2.read()
                             hash = hashlib.sha512()
@@ -397,17 +398,17 @@ def execute_command():
                         if not os.path.isfile("/tmp/tap.txt") or commands == 1:
                             print("[*] New commands identified, sending instructions to TAP.")
                             # write out the new commands
-                            filewrite = file("/tmp/tap.txt", "w")
+                            filewrite = open("/tmp/tap.txt", "w")
                             filewrite.write(html)
                             filewrite.close()
                             time.sleep(1)
-                            fileopen = file("/tmp/tap.txt", "r")
+                            fileopen = open("/tmp/tap.txt", "r")
                             for line in fileopen:
                                 line = line.rstrip()
                                 # don't pull the execute commands line
                                 if line != "EXECUTE COMMANDS":
-				    if line != "EXECUTE COMMAND":
-	                                    subprocess.Popen(line, shell=True).wait()
+                                    if line != "EXECUTE COMMAND":
+                                        subprocess.Popen(line, shell=True).wait()
     
                 # passing to keep it from erroring if Internet was down
                 except: pass
@@ -483,8 +484,8 @@ def check_os():
 #
 def motd(client):
     print ("Updating the MOTD for TAP...")
-    data = file("/usr/share/tap/src/motd.txt", "rb").read()
-    filewrite = file("/etc/motd", "w")
+    data = open("/usr/share/tap/src/motd.txt", "rb").read()
+    filewrite = open("/etc/motd", "w")
     filewrite.write(data)
     filewrite.write("\nTAP Customer Name: %s" % (client))
     filewrite.close()
@@ -499,7 +500,7 @@ def log_everything():
     # if this wasn't added to our config, we will now add it
     if log == None:
         if os.path.isfile("/usr/share/tap/config"):
-            filewrite = file("/usr/share/tap/config", "a")
+            filewrite = open("/usr/share/tap/config", "a")
             # we default to YES
             filewrite.write("# LOG EVERY COMMAND VIA SSH? YES OR NO - ALL LOGS GO TO /var/log/messages\nLOG_EVERYTHING=YES")
             filewrite.close()
@@ -510,13 +511,13 @@ def log_everything():
     log = check_config("LOG_EVERYTHING=")
     if log.lower() == "yes":
         # check to see if its already added here
-        data = file("/etc/bash.bashrc", "r").read()
+        data = open("/etc/bash.bashrc", "r").read()
         if """PROMPT_COMMAND='history -a >(logger -t "$USER[$PWD] $SSH_CONNECTION")'""" in data:
             # already added
             print("[*] Logger already added and working.. All SSH commands are logging.")
         else:
             print("[*] Adding logging capabilities, all results will be logged in /var/log/messages")
-            filewrite = file("/etc/bash.bashrc", "a")
+            filewrite = open("/etc/bash.bashrc", "a")
             filewrite.write("""PROMPT_COMMAND='history -a >(logger -t "$USER[$PWD] $SSH_CONNECTION")'""")
             filewrite.close()
             print("[*] Now log off this current SSH connection and re-login and you will be all set.")        
@@ -524,11 +525,11 @@ def log_everything():
     # if we are set to no, make sure its been removed properly
     if log.lower() == "no":
         # we need to check if its there first
-        fileopen = file("/etc/bash.bashrc", "r")
+        fileopen = open("/etc/bash.bashrc", "r")
         data = fileopen.read()
         if """PROMPT_COMMAND='history -a >(logger -t "$USER[$PWD] $SSH_CONNECTION")'""" in data:
             print("[*] Removing logger and turning it off...")
-            filewrite = file("/etc/bash.bashrc.bak", "w")
+            filewrite = open("/etc/bash.bashrc.bak", "w")
             data = ""
             for line in fileopen:
                 line = line.rstrip()
@@ -547,9 +548,9 @@ def log_everything():
 # update the init.d
 def update_startup():
     # startup script here
-    fileopen = file("/usr/share/tap/src/core/startup_tap", "r")
+    fileopen = open("/usr/share/tap/src/core/startup_tap", "r")
     config = fileopen.read()
-    filewrite = file("/etc/init.d/tap", "w")
+    filewrite = open("/etc/init.d/tap", "w")
     filewrite.write(config)
     filewrite.close()
     print("[*] Triggering update-rc.d on TAP to automatic start...")
@@ -561,11 +562,11 @@ def ssh_vpn():
     if os.path.isfile("/etc/ssh/sshd_config"):
         print("[*] Checking if SSH point-to-point is enabled in SSH config")
         # first we check to see if point-to-point is enabled, if not we will add it
-        data = file("/etc/ssh/sshd_config", "r").read()
+        data = open("/etc/ssh/sshd_config", "r").read()
         # if it isn't lets add
         if not "PermitTunnel point-to-point" in data:
             print("[-] Adding PermitTunnel point-to-point to the SSH config.")
-            filewrite = file("/etc/ssh/sshd_config", "a")
+            filewrite = open("/etc/ssh/sshd_config", "a")
             filewrite.write("\nPermitTunnel point-to-point\n")
             filewrite.close()
             print("[*] Done! Use the SSH vpn script under scripts in TAP source to VPN into host.")
@@ -576,4 +577,4 @@ def ssh_vpn():
 
 # set the background to tap
 def set_background():
-	subprocess.Popen("gconftool -t string -s /desktop/gnome/background/picture_filename src/tap.jpg", shell=True).wait()
+    subprocess.Popen("gconftool -t string -s /desktop/gnome/background/picture_filename src/tap.jpg", shell=True).wait()
